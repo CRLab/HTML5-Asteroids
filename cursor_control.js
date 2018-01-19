673b7322ccec8e6ebe301eddea27a12c39bb6643
David Watkins <djrival7@gmail.com>
2018-01-19 18:08:12 -0500

    Added button bbox

--- cursor_control.js
+++ cursor_control.js
@@ -106,6 +106,11 @@ var Sprite = function () {
   this.preMove  = null;
   this.postMove = null;
 
+  this.r = 0;
+  this.g = 0;
+  this.b = 0;
+  this.alpha = 1;
+
   this.run = function(delta) {
 
     this.move(delta);
@@ -442,6 +447,49 @@ var Ship = function () {
 };
 Ship.prototype = new Sprite();
 
+var ButtonBBox = function () {
+  this.init("button",
+            [-10,   0,
+              -5,   7,
+              -3,   4,
+               1,  10,
+               5,   4,
+              10,   0,
+               5,  -6,
+               2, -10,
+              -4, -10,
+              -4,  -5]);
+
+  this.visible = true;
+  this.scale = 6;
+  this.postMove = this.wrapPostMove;
+
+  this.collidesWith = ["ship", "bullet", "bigalien", "alienbullet"];
+
+  this.collision = function (other) {
+    SFX.explosion();
+    if (other.name == "bullet") Game.score += 120 / this.scale;
+    this.scale /= 3;
+    if (this.scale > 0.5) {
+      // break into fragments
+      for (var i = 0; i < 3; i++) {
+        var roid = $.extend(true, {}, this);
+        roid.vel.x = Math.random() * 6 - 3;
+        roid.vel.y = Math.random() * 6 - 3;
+        if (Math.random() > 0.5) {
+          roid.points.reverse();
+        }
+        roid.vel.rot = Math.random() * 2 - 1;
+        roid.move(roid.scale * 3); // give them a little push
+        Game.sprites.push(roid);
+      }
+    }
+    Game.explosionAt(other.x, other.y);
+    this.die();
+  };
+};
+ButtonBBox.prototype = new Sprite();
+
 var GridNode = function () {
   this.north = null;
   this.south = null;
@@ -590,9 +638,6 @@ $(function () {
     myCanvas.width = window.innerWidth;
     myCanvas.height = window.innerHeight;
     $('#overlay').show();
-    myContext.lineWidth = 3;
-    myContext.strokeStyle = "red";
-    myContext.strokeRect(0,0,myCanvas.width,myCanvas.height);   
 
   var canvas = $("#canvas");
   Game.canvasWidth  = canvas.width();
